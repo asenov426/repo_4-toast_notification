3241cd81464d1a8bece186bdd11ac1e49a877166
asenov426 <asenov.apr26@gmail.com>
2026-04-19 22:45:02 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('btn');
+const toasts = document.getElementById('toasts');
+
+button.addEventListener('click',  function () {
+    const notif = document.createElement('p');
+    notif.classList.add('toast');
+    notif.innerText = "Some information";
+    toasts.appendChild(notif); 
+});
